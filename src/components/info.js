8534f3d883447d3d9af667386352e4fe5bcf742e
Stephen Wolf <wolf7@clemson.edu>
2022-11-17 21:57:26 -0500

    Mostly reorg'd axis

--- src/components/info.js
+++ src/components/info.js
@@ -15,17 +15,9 @@ function StaticDemo() {
   } = useUIState();
 
   return (
-    <div>
-      <div className="h-4/5 w-4/5 aspect-square">
-        <img
-          src={activeVisualization}
-          className="object-scale-down h-3/5 w-3/5"
-          alt="no"
-        />
-      </div>
-
-      <div>
-        <div>Y-Axis</div>
+    <div className="flex flex-row">
+      <div className="flex flex-col h-full w-50">
+        <p>Y-Axis</p>
         <Button
           onClick1={() => selectNewY("ethnicity")}
           onClick2={() => clearSelectionY()}
@@ -59,64 +51,69 @@ function StaticDemo() {
           IC/PM
         </Button>
       </div>
-      <div>
-        <div>X-Axis</div>
-        <Button
-          onClick1={() => selectNewX("age")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.age.selected}
-          enabled={xButtonState.age.enabled}
-        >
-          Age
-        </Button>
-        <Button
-          onClick1={() => selectNewX("compensation")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.compensation.selected}
-          enabled={xButtonState.compensation.enabled}
-        >
-          Compensation
-        </Button>
-        <Button
-          onClick1={() => selectNewX("experience")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.experience.selected}
-          enabled={xButtonState.experience.enabled}
-        >
-          Experience
-        </Button>
-        <Button
-          onClick1={() => selectNewX("gender")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.gender.selected}
-          enabled={xButtonState.gender.enabled}
-        >
-          Gender
-        </Button>
-        <Button
-          onClick1={() => selectNewX("icpm")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.icpm.selected}
-          enabled={xButtonState.icpm.enabled}
-        >
-          IC/PM
-        </Button>
-        <Button
-          onClick1={() => selectNewX("median")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.median.selected}
-          enabled={xButtonState.median.enabled}
-        >
-          Median Compensation
-        </Button>
-        <Button
-          onClick1={() => selectNewX("remote")}
-          onClick2={() => clearSelectionX()}
-          active={xButtonState.remote.selected}
-          enabled={xButtonState.remote.enabled}
-        >
-          Remote Work
-        </Button>
+
+      <div className="flex grow flex-col h-full w-full">
+        <img src={activeVisualization} className="object-contain max-h-fit max-w-fit shrink" alt="no" />
+
+        <div className="w-full h-50">
+          <p>X-Axis</p>
+          <Button
+            onClick1={() => selectNewX("age")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.age.selected}
+            enabled={xButtonState.age.enabled}
+          >
+            Age
+          </Button>
+          <Button
+            onClick1={() => selectNewX("compensation")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.compensation.selected}
+            enabled={xButtonState.compensation.enabled}
+          >
+            Compensation
+          </Button>
+          <Button
+            onClick1={() => selectNewX("experience")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.experience.selected}
+            enabled={xButtonState.experience.enabled}
+          >
+            Experience
+          </Button>
+          <Button
+            onClick1={() => selectNewX("gender")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.gender.selected}
+            enabled={xButtonState.gender.enabled}
+          >
+            Gender
+          </Button>
+          <Button
+            onClick1={() => selectNewX("icpm")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.icpm.selected}
+            enabled={xButtonState.icpm.enabled}
+          >
+            IC/PM
+          </Button>
+          <Button
+            onClick1={() => selectNewX("median")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.median.selected}
+            enabled={xButtonState.median.enabled}
+          >
+            Median Compensation
+          </Button>
+          <Button
+            onClick1={() => selectNewX("remote")}
+            onClick2={() => clearSelectionX()}
+            active={xButtonState.remote.selected}
+            enabled={xButtonState.remote.enabled}
+          >
+            Remote Work
+          </Button>
+        </div>
       </div>
     </div>
   );
